2533023efb9bdf602c78f58d58bdfd1976cb111e
GIDEONZIA <gideonkahindi9329@gmail.com>
2026-05-10 08:46:41 +0300

    RentFlow MVP complete: models, migrations, admin, superuser

--- apps/core/apps.py
+++ apps/core/apps.py
@@ -1,5 +1,6 @@
+# apps/core/apps.py
 from django.apps import AppConfig
 
-
 class CoreConfig(AppConfig):
-    name = 'core'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.core'  # <--- This MUST include 'apps.'
--- config/settings/production.py
+++ config/settings/production.py
@@ -4,7 +4,6 @@ from .base import *
 DEBUG = False
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['.rentflow.co.ke'])
 
-# Security
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
@@ -13,7 +12,6 @@ SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 
-# Logging
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
